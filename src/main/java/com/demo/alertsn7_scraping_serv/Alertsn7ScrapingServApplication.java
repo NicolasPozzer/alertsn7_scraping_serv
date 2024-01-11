3673ec25c441663d6408bcbb33b92711263efc5b
NicolasPozzer <pozzernico@gmail.com>
2024-01-11 18:49:29 -0300

    publish in git

---
+++ src/main/java/com/demo/alertsn7_scraping_serv/Alertsn7ScrapingServApplication.java
@@ -0,0 +1,13 @@
+package com.demo.alertsn7_scraping_serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alertsn7ScrapingServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alertsn7ScrapingServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/alertsn7_scraping_serv/Alertsn7ScrapingServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.alertsn7_scraping_serv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alertsn7ScrapingServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
